615902ff933311dc4746a077f6d1641b692c625f
stephen grider <ste.grider@gmail.com>
2016-03-03 09:53:33 -0800

    added first spec

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -2,10 +2,17 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // Use 'describe' to group together similar tests
-describe('App')
+describe('App', () => {
 
-// Use 'it' to test a single attribute of a target
-it('shows the correct text')
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-// Use 'expect' to make an 'assertion' about a target
-expect
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+
+  });
+
+});
