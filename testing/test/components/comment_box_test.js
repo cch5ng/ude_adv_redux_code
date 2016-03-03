f466d77e9d3040c5cff836b92e7adac67285b026
stephen grider <ste.grider@gmail.com>
2016-03-03 12:44:26 -0800

    refactored code with a beforeeach

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -2,18 +2,21 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
