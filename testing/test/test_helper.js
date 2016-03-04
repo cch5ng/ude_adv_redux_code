040f4c41531525e2c2483d1d403a62257ae77bb2
stephen grider <ste.grider@gmail.com>
2016-03-03 18:18:21 -0800

    added jquery

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -1,8 +1,10 @@
-
+import jsdom from 'jsdom';
+import jquery from 'jquery';
 
 // Set up testing environment to run like a browser in the command line
-
-
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
 
