d2045e1171e0372d8d5bf252e829e4df7d2c2da3
stephen grider <ste.grider@gmail.com>
2016-03-03 17:39:59 -0800

    finished app

--- testing/src/components/comment_box.js
+++ testing/src/components/comment_box.js
@@ -23,10 +23,13 @@ class CommentBox extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <h4>Add a comment</h4>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)} />
-        <button action="submit">Submit Comment</button>
+        <div>
+          <button action="submit">Submit Comment</button>
+        </div>
       </form>
     );
   }
