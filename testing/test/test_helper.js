c5ee154b3868cfc12855ee6ad1b44718290933f5
stephen grider <ste.grider@gmail.com>
2016-03-03 18:34:50 -0800

    added test utils

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -1,5 +1,7 @@
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -7,8 +9,11 @@ global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
+function renderComponent(ComponentClass) {
+  const componentInstance = TestUtils.renderIntoDocument(<ComponentClass />);
 
-
+  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+}
 
 // Build helper for simulating events
 
