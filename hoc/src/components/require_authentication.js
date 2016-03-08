7a8236cb883e891aaf27f0e4b85c3b10a01c07e3
stephen grider <ste.grider@gmail.com>
2016-03-08 13:48:02 -0800

    Added lifecycle methods

--- hoc/src/components/require_authentication.js
+++ hoc/src/components/require_authentication.js
@@ -7,8 +7,19 @@ export default function(ComposedComponent) {
       router: React.PropTypes.object
     }
 
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      console.log(this.context);
       return <ComposedComponent {...this.props} />
     }
   }
