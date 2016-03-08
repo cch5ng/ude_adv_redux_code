5cb0c34780cceb31e83244370c244360549a023e
stephen grider <ste.grider@gmail.com>
2016-03-08 13:33:48 -0800

    added contexttypes

--- hoc/src/components/require_authentication.js
+++ hoc/src/components/require_authentication.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
     render() {
-      console.log(this.props.authenticated);
+      console.log(this.context);
       return <ComposedComponent {...this.props} />
     }
   }
