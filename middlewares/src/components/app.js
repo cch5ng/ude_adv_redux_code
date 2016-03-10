4066a13beef7e02609cb84c2e01d73d90511a883
stephen grider <ste.grider@gmail.com>
2016-03-09 16:55:18 -0800

    added list of users

--- middlewares/src/components/app.js
+++ middlewares/src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
+import UserList from './user_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <UserList />
+      </div>
     );
   }
 }
