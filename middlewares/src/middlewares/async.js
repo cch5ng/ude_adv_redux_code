70ad4151dc54243b8e02801fa0fb7e3f71d87f92
stephen grider <ste.grider@gmail.com>
2016-03-09 17:58:17 -0800

    added base case to middleware

--- middlewares/src/middlewares/async.js
+++ middlewares/src/middlewares/async.js
@@ -1,7 +1,12 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // If action does not have payload
+    // or, the payload does not have a .then property
+    // we dont care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    next(action);
+    console.log('We dont have a promise', action);
   }
 }
