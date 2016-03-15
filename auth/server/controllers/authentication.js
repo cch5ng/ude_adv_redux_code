e71e4bde8912ff17b96bac21de9cf0df56177491
stephen grider <ste.grider@gmail.com>
2016-03-15 11:44:43 -0700

    create users

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -6,12 +6,24 @@ exports.signup = function(req, res, next) {
 
   // See if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+    // If a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Repond to request indicating the user was created
+      res.json({ success: true });
+    });
   });
-
-  // If a user with email does exist, return an error
-
-  // If a user with email does NOT exist, create and save user record
-
-  // Repond to request indicating the user was created
 }
