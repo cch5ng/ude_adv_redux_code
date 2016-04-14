0af185117e0205f3a5a39b6817a37f0e3b3f06c0
stephen grider <ste.grider@gmail.com>
2016-04-14 15:59:33 -0700

    signin form

--- auth/client/src/components/app.js
+++ auth/client/src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+        React simple starter
+      </div>
     );
   }
 }
