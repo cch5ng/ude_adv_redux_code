bf64c5ea8e6ae00358bfa0eba3cde897a863db94
stephen grider <ste.grider@gmail.com>
2016-04-15 16:12:49 -0700

    added error type

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
+import {
+  AUTH_USER,
+  AUTH_ERROR
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -10,13 +14,23 @@ export function signinUser({ email, password }) {
       .then(response => {
         // If request is good...
         // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
         // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
         // - redirect to the route '/feature'
         browserHistory.push('/feature');
       })
       .catch(() => {
         // If request is bad...
         // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
       });
   }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
