5f25ac8dd01478a7700011771bc96925a65ef31a
stephengrider <ste.grider@gmail.com>
2018-05-15 15:48:48 -0600

    trick jsdom

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
 
-it('shows a comment box', () => {});
+it('shows a comment box', () => {
+  const div = document.create('div');
+
+  ReactDOM.render(<App />, div);
+
+  // Looks inside the div
+  // and checks to see if the CommentBox is in there
+
+  ReactDOM.unmountComponentAtNode(div);
+});
