bde7e669eec591010835883c1c2986bd581be393
stephengrider <ste.grider@gmail.com>
2018-05-15 16:03:26 -0600

    verify

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
-  const div = document.create('div');
+  const div = document.createElement('div');
 
   ReactDOM.render(<App />, div);
 
   // Looks inside the div
   // and checks to see if the CommentBox is in there
+  expect(div.innerHTML).toContain('Comment Box');
 
   ReactDOM.unmountComponentAtNode(div);
 });
