3fb89c5d5db6b3f548bbb9e1c193a0e131397777
stephengrider <ste.grider@gmail.com>
2018-05-15 13:31:13 -0600

    test

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -6,7 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 
-  expect(div.innerHTML).toContain('Welcome to Not React!!');
+  expect(div.innerHTML).toContain('Hi there!');
 
   ReactDOM.unmountComponentAtNode(div);
 });
