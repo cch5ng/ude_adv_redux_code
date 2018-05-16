1e737dd09304ba6ce150169f44f78a116dd27bb1
stephengrider <ste.grider@gmail.com>
2018-05-16 13:58:33 -0600

    solution

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -25,3 +25,14 @@ it('has a text area that users can type in', () => {
 
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
 });
+
+it('when form is submitted, text area gets emptied', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
