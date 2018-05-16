cff16db04534b996680caf45a16ac0829561c3f1
stephengrider <ste.grider@gmail.com>
2018-05-16 15:25:27 -0600

    comment box gets redux

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -10,9 +12,7 @@ class CommentBox extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    // TODO - Call an action creator
-    // And save the comment
-
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   };
 
@@ -29,4 +29,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
