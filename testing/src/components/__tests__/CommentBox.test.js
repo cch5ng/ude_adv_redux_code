c5bc5dd75cb7f3379bd8146d2aaf05a5aee9a70b
stephengrider <ste.grider@gmail.com>
2018-05-16 13:38:21 -0600

    fake events

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,9 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+});
