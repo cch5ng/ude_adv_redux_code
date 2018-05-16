96c011d46ab58096d7c9e397f40cf933648c0790
stephengrider <ste.grider@gmail.com>
2018-05-16 13:07:16 -0600

    aftereach statements

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
