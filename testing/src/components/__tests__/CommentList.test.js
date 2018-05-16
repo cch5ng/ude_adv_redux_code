4989b2b5291f1c50a2c789c567e1330b7d54e6e4
stephengrider <ste.grider@gmail.com>
2018-05-16 17:29:33 -0600

    initial state

--- testing/src/components/__tests__/CommentList.test.js
+++ testing/src/components/__tests__/CommentList.test.js
@@ -7,11 +7,17 @@ import Root from 'Root';
 let wrapped;
 
 beforeEach(() => {
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2']
+  };
+
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   );
 });
 
-it('creates one LI per comment', () => {});
+it('creates one LI per comment', () => {
+  console.log(wrapped.find('li').length);
+});
