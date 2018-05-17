95b977776b3f8b450ea2aee1e394554844d6e9ed
stephengrider <ste.grider@gmail.com>
2018-05-17 12:35:06 -0600

    moxios wait

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -25,12 +25,12 @@ it('can fetch a list of comments and display them', done => {
 
   wrapped.find('.fetch-comments').simulate('click');
 
-  setTimeout(() => {
+  moxios.wait(() => {
     wrapped.update();
 
-    expect(wrapped.find('li').length).toEqual(3);
+    expect(wrapped.find('li').length).toEqual(2);
 
     done();
     wrapped.unmount();
-  }, 100);
+  });
 });
