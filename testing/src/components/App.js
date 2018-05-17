5ea09988da0b35228ee170d21b8b3dee21726647
stephengrider <ste.grider@gmail.com>
2018-05-17 16:33:21 -0600

    added header

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+export default class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post A Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
