6c874adaa1826542d3810f7afa731edae55ddc06
stephengrider <ste.grider@gmail.com>
2018-05-17 11:04:21 -0600

    fix a test

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -5,6 +5,13 @@ import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the *entire* app
+  const wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+
   // find the 'fetchComments' button and click it
+
   // Expect to find a list of comments!
 });
