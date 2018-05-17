7a93b8ec3260df0864c2d9dbfebe58bd2ca6cca4
stephengrider <ste.grider@gmail.com>
2018-05-17 11:25:58 -0600

    fake reqs with moxios

--- testing/src/__tests__/integrations.test.js
+++ testing/src/__tests__/integrations.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }]
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the *entire* app
@@ -15,5 +28,5 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to find a list of comments!
-  expect(wrapped.find('li').length).toEqual(500);
+  expect(wrapped.find('li').length).toEqual(2);
 });
