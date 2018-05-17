5c3a4cafd399abf94b925c15ce6f3ae328a5a021
stephengrider <ste.grider@gmail.com>
2018-05-17 17:54:27 -0600

    move logic

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -5,22 +5,6 @@ import * as actions from 'actions';
 class CommentBox extends Component {
   state = { comment: '' };
 
-  // Our component just got rendered
-  componentDidMount() {
-    this.shouldNavigateAway();
-  }
-
-  // Our component just got updated
-  componentDidUpdate() {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      this.props.history.push('/');
-    }
-  }
-
   handleChange = event => {
     this.setState({ comment: event.target.value });
   };
@@ -50,8 +34,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
