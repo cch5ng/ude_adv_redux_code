0a5e1c4c67ab914063b74b4bb20f88c6b30f0f8d
stephengrider <ste.grider@gmail.com>
2018-05-18 14:08:28 -0600

    warn

--- middleware/src/middlewares/stateValidator.js
+++ middleware/src/middlewares/stateValidator.js
@@ -4,5 +4,7 @@ import stateSchema from './stateSchema';
 export default ({ dispatch, getState }) => next => action => {
   next(action);
 
-  console.log(tv4.validate(getState(), stateSchema));
+  if (!tv4.validate(getState(), stateSchema)) {
+    console.warn('Invalid state schema detected');
+  }
 };
