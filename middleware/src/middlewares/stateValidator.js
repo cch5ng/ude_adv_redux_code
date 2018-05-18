bc2baa4428a81343b993cec930107b2afbf24476
stephengrider <ste.grider@gmail.com>
2018-05-18 14:02:08 -0600

    added middleware

---
+++ middleware/src/middlewares/stateValidator.js
@@ -0,0 +1,8 @@
+import tv4 from 'tv4';
+import stateSchema from './stateSchema';
+
+export default ({ dispatch, getState }) => next => action => {
+  next(action);
+
+  console.log(tv4.validate(getState(), stateSchema));
+};
