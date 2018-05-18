fa85dd8d62f275eb92469d2df6650819e037ef41
stephengrider <ste.grider@gmail.com>
2018-05-18 12:15:57 -0600

    wait for promise resolution

--- middleware/src/middlewares/async.js
+++ middleware/src/middlewares/async.js
@@ -7,4 +7,12 @@ export default ({ dispatch }) => next => action => {
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
+
+  // We want to wait for the promise to resolve
+  // (get its data!!) and then create a new action
+  // with that data and dispatch it
+  action.payload.then(function(response) {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
