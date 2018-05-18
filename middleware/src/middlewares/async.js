fe365b089ca7409400a5ae06a2eaf541b2832556
stephengrider <ste.grider@gmail.com>
2018-05-18 12:05:56 -0600

    forward the action

--- middleware/src/middlewares/async.js
+++ middleware/src/middlewares/async.js
@@ -1,1 +1,10 @@
-export default ({ dispatch }) => next => action => {};
+export default ({ dispatch }) => next => action => {
+  // Check to see if the action
+  // has a promise on its 'payload' property
+  // If it does, then wait for it to resolve
+  // If it doesn't, then send the action on to the
+  // next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+};
