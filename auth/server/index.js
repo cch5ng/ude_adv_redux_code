85b5123510f2b500c5dd0d0903b3c543523ddc71
stephengrider <ste.grider@gmail.com>
2018-05-21 12:34:57 -0600

    cors middleware

--- auth/server/index.js
+++ auth/server/index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect('mongodb://localhost/auth');
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
