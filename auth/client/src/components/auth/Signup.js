2f75877521c0e4067ebd5a06fe2696e70baaf656
stephengrider <ste.grider@gmail.com>
2018-05-21 11:23:50 -0600

    compose

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 class Signup extends Component {
   onSubmit = formProps => {
-    console.log(formProps);
+    this.props.signup(formProps);
   };
 
   render() {
@@ -37,4 +38,6 @@ class Signup extends Component {
   }
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(connect(null, actions), reduxForm({ form: 'signup' }))(
+  Signup
+);
