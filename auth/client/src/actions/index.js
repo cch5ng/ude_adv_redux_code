f89c463651c78028eaafb4a8a9ed170245e6f42b
stephengrider <ste.grider@gmail.com>
2018-05-21 14:08:14 -0600

    redirect on signup

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = formProps => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
       'http://localhost:3090/signup',
@@ -9,6 +9,7 @@ export const signup = formProps => async dispatch => {
     );
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
   } catch (e) {
     dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
   }
