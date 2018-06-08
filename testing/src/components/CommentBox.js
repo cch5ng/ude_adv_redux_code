9e8e78c20bfe52a8a256d1cdc59eb7e59f57450e
Carol Chung <cch5ng@gmail.com>
2018-06-08 15:31:38 -0700

    feat: fill in hoc with reusable logic

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from '../components/requireAuth';
 
 class CommentBox extends Component {
   state = { comment: '' };
-
-  // Our component just got rendered
-  componentDidMount() {
-    this.shouldNavigateAway();
-  }
-
-  // Our component just got updated
-  componentDidUpdate() {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway() {
-    if (!this.props.auth) {
-      this.props.history.push('/');
-    }
-  }
 
   handleChange = event => {
     this.setState({ comment: event.target.value });
@@ -50,8 +35,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { auth: state.auth };
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
--- testing/src/components/requireAuth.js
+++ testing/src/components/requireAuth.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default ChildComponent => {
   class ComposedComponent extends Component {
+    // to abstract in hoc
+    // Our component just got rendered
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    // to abstract in hoc
+    // Our component just got updated
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    // to abstract in hoc
+    shouldNavigateAway() {
+      if (!this.props.auth) {
+        this.props.history.push('/');
+      }
+    }
+
     render() {
       return <ChildComponent />;
     }
   }
 
-  return ComposedComponent;
+  // to abstract in hoc
+  function mapStateToProps(state) {
+    return { auth: state.auth };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };
+
+/*
+
+ex usage ex from CommentBox
+
+import requireAuth from './components/requireAuth';
+
+export default requireAuth(CommentBox);
+
+*/
