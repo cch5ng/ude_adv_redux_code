8958f43df7e95904eabfa669b55f8bc22dfbfc2c
Carol Chung <cch5ng@gmail.com>
2018-06-09 21:21:37 -0700

    init server setup

---
+++ auth/server/index.js
@@ -0,0 +1,14 @@
+// main start point
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// app setup
+
+// server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on port:', port);
