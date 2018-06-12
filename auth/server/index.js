52e84481fb5157b79081d32bf1ad2e1277fed52b
Carol Chung <cch5ng@gmail.com>
2018-06-11 22:28:17 -0700

    feat: middleware and router

--- auth/server/index.js
+++ auth/server/index.js
@@ -4,8 +4,15 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // app setup
+// morgan and bodyParser are middleware
+// morgan is used to log server events to server terminal
+app.use(morgan('combined'));
+// bodyParser parses incoming request into json
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // server setup
 const port = process.env.PORT || 3090;
