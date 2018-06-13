45c49ed21c3d9e304c8c09054e59340b2e67eab9
Carol Chung <cch5ng@gmail.com>
2018-06-12 23:17:03 -0700

    refactor: handle route with controller

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,10 +1,13 @@
+const Authentication = require('./controllers/authentication');
+
 // export a function that has access to express app
-
 module.exports = function(app) {
-  // handler for get requests made to express app
-  app.get('/', function(req, res, next) {
-    res.send(['bottle', 'phone', 'paper']);
+  app.post('/signup', Authentication.signup);
 
-  });
+
+  // handler for get requests made to express app
+  // app.get('/', function(req, res, next) {
+  //   res.send(['bottle', 'phone', 'paper']);
+  // });
 
 }
