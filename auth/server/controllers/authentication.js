9bd3b028ec73ce0d846f1a02560ff41c6b608645
Carol Chung <cch5ng@gmail.com>
2018-06-13 16:39:24 -0700

    feat: return jwt with successful auth

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   // check if user exists (email/pwd)
@@ -28,7 +35,7 @@ exports.signup = function(req, res, next) {
       user.save(function(err, user) {
         if (err) return next(err);
         // return resp
-        res.json({ success: true });
+        res.json({ token: tokenForUser(user) });
       });
     }
   })
