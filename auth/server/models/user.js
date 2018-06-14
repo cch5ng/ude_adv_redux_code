22be240541667772a44705c137d4fbfd55fad8f8
Carol Chung <cch5ng@gmail.com>
2018-06-14 15:51:15 -0700

    add helper to compare login pwd with db pwd

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -26,6 +26,15 @@ userSchema.pre('save', function(next) {
   })
 })
 
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return cb(err) }
+
+    cb(null, isMatch);
+  })
+}
+
+
 // create model class
 const ModelClass = mongoose.model('user', userSchema);
 
